0c96b5675c4399de819f120dd93ba0cb3b95ed78
code.arpit <code.arpitjain@gmail.com>
2021-04-30 21:34:24 +0530

    color channels (b,g,r)

---
+++ Advanced/Color_channels.py
@@ -0,0 +1,26 @@
+import cv2 as cv 
+import numpy as np
+
+img = cv.imread('/home/arpitjain/Desktop/Code/Python/OpenCV/pictures/falls.jpg')
+image = cv.resize(img, (700,500),interpolation=cv.INTER_AREA)
+cv.imshow('image', image)
+
+b,g,r = cv.split(image)
+
+cv.imshow('blue', b)
+cv.imshow('green', g)
+cv.imshow('red', r)
+
+print(image.shape)
+print(b.shape)
+print(g.shape)
+print(r.shape)
+
+merged = cv.merge([b,g,r])
+cv.imshow('merged', merged)
+
+blank = np.zeros(image.shape[0:2], dtype='uint8')
+
+
+
+cv.waitKey
