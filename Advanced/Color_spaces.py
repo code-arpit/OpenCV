9bc45a0871678cf51de9b40f081bc0e069950e14
code.arpit <code.arpitjain@gmail.com>
2021-04-30 20:26:56 +0530

    nnew functions

--- Advanced/Color_spaces.py
+++ Advanced/Color_spaces.py
@@ -2,8 +2,23 @@ import cv2 as cv
 
 img = cv.imread('/home/arpitjain/Desktop/Code/Python/OpenCV/pictures/falls.jpg')
 image = cv.resize(img, (700,500),interpolation=cv.INTER_AREA)
+# cv.imshow('image', image)
 
+#....BGR to Grayscale
 gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-cv.imshow('gray', gray)
+# cv.imshow('gray', gray)
+
+#....BGR to HSV
+hsv = cv.cvtColor(image, cv.COLOR_BGR2HSV)
+# cv.imshow('hsv', hsv)
+
+#....BGR to L*a*b
+lab = cv.cvtColor(image, cv.COLOR_BGR2LAB)
+# cv.imshow('lab', lab)
+
+rgb = cv.cvtColor(image, cv.COLOR_BGR2RGB)
+cv.imshow('rgb', rgb)
+
+
 
 cv.waitKey(0)
