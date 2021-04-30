972e850edd145f1eeadcbc2abd30791fbbc3d2b7
code.arpit <code.arpitjain@gmail.com>
2021-04-30 20:47:08 +0530

    Basics of OpenCV python

---
+++ Basics/basic_functions.py
@@ -0,0 +1,47 @@
+import cv2 as cv
+
+
+def rescale_frame(frame, scale):
+    width = int(frame.shape[1] * scale)
+    height = int(frame.shape[0] * scale)
+    dimensions = (width, height)
+
+    return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
+
+img = cv.imread('/home/arpitjain/Desktop/Code/Python/opencv/pictures/falls.jpg')
+
+#....rescale
+img_rescale = rescale_frame(img, 0.25)
+cv.imshow('falls', img_rescale)
+
+#....converting to grayscale
+# gray = cv.cvtColor(img_rescale, cv.COLOR_BGR2GRAY)
+# cv.imshow('gray', gray)
+
+#....BLur
+# blur = cv.GaussianBlur(img_rescale, (9,9), cv.BORDER_DEFAULT)
+# cv.imshow('blur',blur)
+
+#....edge cascade
+# canny = cv.Canny(img_rescale, 175, 175)
+# cv.imshow('canny',canny)
+
+#....dilating image
+# dilated = cv.dilate(canny, (3,3), iterations=9)
+#cv.imshow('dilates', dilated)
+
+#....eroding
+# eroded = cv.erode(canny, (3,3), iterations=3)
+# cv.imshow('eroded', eroded)
+
+#....resize
+resized = cv.resize(img_rescale, (500,300), interpolation=cv.INTER_AREA)
+cv.imshow('resized', resized)
+
+#....cropping
+# cropped = img_rescale[0:100,100:200]
+# cv.imshow('cropped', cropped)
+
+
+
+cv.waitKey(0)
---
+++ Basics/resize_rescale.py
@@ -0,0 +1,34 @@
+import cv2 as cv
+
+#rescale (Used for images, videos , live video)
+def rescaleFrame(frame, scale):
+    width = int(frame.shape[1] * scale)
+    height = int(frame.shape[0] * scale)
+    dimensions = (width,height)
+
+    return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
+#....resize images
+
+# img = cv.imread('/home/arpitjain/Desktop/Code/Python/opencv/pictures/falls.jpg')
+#
+# img_resized = rescaleFrame(img,0.2)
+#
+# cv.imshow('falls', img_resized)
+#
+# cv.waitKey(0)
+
+
+#.....resize videos
+# vid = cv.VideoCapture('/home/arpitjain/Desktop/Code/Python/opencv/videos/dog.mp4')
+#
+# while(True):
+#     isTrue, frame = vid.read()
+#
+#     frame_resized =rescaleFrame(frame, 0.25)
+#     cv.imshow('vid_resized', frame_resized)
+#
+#     if cv.waitKey(50) & 0xFF==ord('q'):
+#         break
+#
+# vid.release()
+# cv.destroyAllWindows()
