1919578373970f51056fed8432e8dce26aa6ed19
code.arpit <code.arpitjain@gmail.com>
2021-05-01 11:47:25 +0530

    new changes

--- Advanced/Color_channels.py
+++ Advanced/Color_channels.py
@@ -3,13 +3,13 @@ import numpy as np
 
 img = cv.imread('/home/arpitjain/Desktop/Code/Python/OpenCV/pictures/falls.jpg')
 image = cv.resize(img, (700,500),interpolation=cv.INTER_AREA)
-cv.imshow('image', image)
+# cv.imshow('image', image)
 
 b,g,r = cv.split(image)
 
-cv.imshow('blue', b)
-cv.imshow('green', g)
-cv.imshow('red', r)
+# cv.imshow('blue', b)
+# cv.imshow('green', g)
+# cv.imshow('red', r)
 
 print(image.shape)
 print(b.shape)
@@ -17,15 +17,15 @@ print(g.shape)
 print(r.shape)
 
 merged = cv.merge([b,g,r])
-cv.imshow('merged', merged)
+# cv.imshow('merged', merged)
 
 blank = np.zeros(image.shape[0:2], dtype='uint8')
 blue = cv.merge([b,blank,blank])
 green = cv.merge([blank,g,blank])
 red = cv.merge([blank,blank,r])
 
-cv.imshow('blue', blue)
-cv.imshow('green', green)
-cv.imshow('red', red)
+# cv.imshow('blue', blue)
+# cv.imshow('green', green)
+# cv.imshow('red', red)
 
 cv.waitKey
