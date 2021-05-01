f9dc90b2575251ca0f386583b6d1cc1477137a7d
code.arpit <code.arpitjain@gmail.com>
2021-05-01 14:32:34 +0530

    some Additions

--- Advanced/bitwise_operators.py
+++ Advanced/bitwise_operators.py
@@ -1,14 +1,29 @@
 import cv2 as cv
 import numpy as np
 
-blank = np.zeros((400,600), dtype='uint8')
+blank = np.zeros((400,400), dtype='uint8')
 
 rectangle = cv.rectangle(blank.copy(), (30,30), (370,370), 255, -1)
 circle = cv.circle(blank.copy(), (200,200), 200, 255, -1)
-cv.imshow('rectangle',rectangle)
-cv.imshow('circle', circle)
+# cv.imshow('rectangle',rectangle)
+# cv.imshow('circle', circle)
 
 #....bitwise AND
+bitwise_AND= cv.bitwise_and(rectangle, circle)
+cv.imshow('bitwise AND', bitwise_AND)
 
+#....bitwise OR
+bitwise_OR = cv.bitwise_or(rectangle, circle)
+cv.imshow('bitwise OR', bitwise_OR)
+
+#....bitwise XOR
+bitwise_XOR = cv.bitwise_xor(rectangle, circle)
+cv.imshow('bitwise XOR', bitwise_XOR)
+
+#....bitwise NOT
+bitwise_NOT = cv.bitwise_not(rectangle)
+cv.imshow('bitwise NOT', bitwise_NOT)
+bitwise_NOT2 = cv.bitwise_not(circle)
+cv.imshow('bitwise NOT', bitwise_NOT2)
 
 cv.waitKey(0)
