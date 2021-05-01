bb89ccfc9b20b28e486d8ce3b4b2d8ad7dfb52e7
code.arpit <code.arpitjain@gmail.com>
2021-05-01 18:57:01 +0530

    masking code

---
+++ Advanced/masking.py
@@ -0,0 +1,16 @@
+import cv2 as cv
+import numpy as np
+
+img = cv.imread('/home/arpitjain/Desktop/Code/Python/OpenCV/pictures/day.jpg')
+# cv.imshow('image', img)
+
+blank = np.zeros(img.shape[0:2], dtype='uint8')
+
+mask = cv.circle(blank, (img.shape[1]//2,img.shape[0]//2),150, 255, -1)
+# cv.imshow('mask', mask)
+
+masked = cv.bitwise_and(img, img, mask=mask)
+cv.imshow('masked', masked)
+
+
+cv.waitKey(0)
